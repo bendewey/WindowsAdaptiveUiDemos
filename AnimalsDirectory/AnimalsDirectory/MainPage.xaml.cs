2b5e673216b5ec9abc1183c05c2c37a08f097acb
Ben Dewey <bdewey01@hotmail.com>
2015-10-16 13:42:13 -0400

    Added Master/Detail sample (AnimalDirectory)

---
+++ AnimalsDirectory/AnimalsDirectory/MainPage.xaml.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace AnimalsDirectory
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private Animal _lastSelectedItem;
+
+        public List<Animal> Animals { get; set; }
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            Animals = AnimalsDataSource.GetAnimals();
+        }
+        
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            var items = MasterListView.ItemsSource as List<Animal>;
+
+            if (e.Parameter != null && e.Parameter is int)
+            {
+                // Parameter is item ID
+                var id = (int)e.Parameter;
+                _lastSelectedItem =
+                    items.Where((item) => item.Id == id).FirstOrDefault();
+            }
+
+            UpdateForVisualState(AdaptiveStates.CurrentState);
+
+            // Don't play a content transition for first item load.
+            // Sometimes, this content will be animated as part of the page transition.
+            DisableContentTransitions();
+        }
+
+        private void AdaptiveStates_CurrentStateChanged(object sender, VisualStateChangedEventArgs e)
+        {
+            UpdateForVisualState(e.NewState, e.OldState);
+        }
+
+        private void UpdateForVisualState(VisualState newState, VisualState oldState = null)
+        {
+            var isNarrow = newState == NarrowState;
+
+            if (isNarrow && oldState == DefaultState && _lastSelectedItem != null)
+            {
+                // Resize down to the detail item. Don't play a transition.
+                Frame.Navigate(typeof(DetailPage), _lastSelectedItem.Id, new SuppressNavigationTransitionInfo());
+            }
+
+            EntranceNavigationTransitionInfo.SetIsTargetElement(MasterListView, isNarrow);
+            if (DetailContentPresenter != null)
+            {
+                EntranceNavigationTransitionInfo.SetIsTargetElement(DetailContentPresenter, !isNarrow);
+            }
+        }
+
+        private void MasterListView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            var clickedItem = (Animal)e.ClickedItem;
+            _lastSelectedItem = clickedItem;
+
+            if (AdaptiveStates.CurrentState == NarrowState)
+            {
+                // Use "drill in" transition for navigating from master list to detail view
+                Frame.Navigate(typeof(DetailPage), clickedItem.Id, new DrillInNavigationTransitionInfo());
+            }
+            else
+            {
+                // Play a refresh animation when the user switches detail items.
+                EnableContentTransitions();
+            }
+        }
+
+        private void LayoutRoot_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Assure we are displaying the correct item. This is necessary in certain adaptive cases.
+            MasterListView.SelectedItem = _lastSelectedItem;
+        }
+
+        private void EnableContentTransitions()
+        {
+            DetailContentPresenter.ContentTransitions.Clear();
+            DetailContentPresenter.ContentTransitions.Add(new EntranceThemeTransition());
+        }
+
+        private void DisableContentTransitions()
+        {
+            if (DetailContentPresenter != null)
+            {
+                DetailContentPresenter.ContentTransitions.Clear();
+            }
+        }
+    }
+
+    public class Animal
+    {
+        public string Name;
+
+        public string Description { get; internal set; }
+        public int Id { get; internal set; }
+        public string Image { get; internal set; }
+    }
+
+    public class AnimalsDataSource
+    {
+        private static readonly List<Animal> _animals = new List<Animal>
+            {
+                new Animal() { Id = 1, Name = "Owl", Image = "/Assets/owl.png", Description = "Owls are birds from the order Strigiformes, which includes about 200 species of mostly solitary and nocturnal birds of prey typified by an upright stance, a large, broad head, binocular vision, binaural hearing, sharp talons and feathers adapted for silent flight. Exceptions include the diurnal northern hawk-owl and the gregarious burrowing owl." },
+                new Animal() { Id = 2, Name = "Dog", Image = "/Assets/dog.jpg", Description = "The domestic dog (Canis lupus familiaris or Canis familiaris) is a domesticated canid which has been selectively bred for millennia for various behaviors, sensory capabilities, and physical attributes." },
+                new Animal() { Id = 3, Name = "Cat", Image = "/Assets/cat.jpg", Description = "The domestic cat[1][2] (Felis catus or Felis silvestris catus)[2][4] is a small, usually furry, domesticated, and carnivorous mammal. They are often called housecats when kept as indoor pets or simply cats when there is no need to distinguish them from other felids and felines.[6] Cats are often valued by humans for companionship and their ability to hunt vermin." },
+                new Animal() { Id = 4, Name = "Chicken", Image = "/Assets/chicken.png", Description = "The chicken (Gallus gallus domesticus) is a domesticated fowl, a subspecies of the red junglefowl. As one of the most common and widespread domestic animals, with a population of more than 24 billion in 2003,[1] there are more chickens in the world than any other species of bird. Humans keep chickens primarily as a source of food, consuming both their meat and their eggs." },
+                new Animal() { Id = 5, Name = "Duck", Image = "/Assets/duck.png", Description = "Duck is the common name for a large number of species in the Anatidae family of birds which also includes swans and geese. The ducks are divided among several subfamilies in the Anatidae family; they do not represent a monophyletic group (the group of all descendants of a single common ancestral species) but a form taxon, since swans and geese are not considered ducks. Ducks are mostly aquatic birds, mostly smaller than the swans and geese, and may be found in both fresh water and sea water." },
+                new Animal() { Id = 6, Name = "Rabbit", Image = "/Assets/rabbit.jpg", Description = "Rabbits are small mammals in the family Leporidae of the order Lagomorpha, found in several parts of the world. There are eight different genera in the family classified as rabbits, including the European rabbit (Oryctolagus cuniculus), cottontail rabbits (genus Sylvilagus; 13 species), and the Amami rabbit (Pentalagus furnessi, an endangered species on Amami Ōshima, Japan). There are many other species of rabbit, and these, along with pikas and hares, make up the order Lagomorpha. The male is called a buck and the female is a doe; a young rabbit is a kitten or kit." },
+                new Animal() { Id = 7, Name = "Lion", Image = "/Assets/lion.png", Description = "The lion (Panthera leo) is one of the five big cats in the genus Panthera and a member of the family Felidae. The commonly used term African lion collectively denotes the several subspecies found in Africa. With some males exceeding 250 kg (550 lb) in weight,[4] it is the second-largest living cat after the tiger. Wild lions currently exist in sub-Saharan Africa and in Asia (where an endangered remnant population resides in Gir Forest National Park in India) while other types of lions have disappeared from North Africa and Southwest Asia in historic times. Until the late Pleistocene, about 10,000 years ago, the lion was the most widespread large land mammal after humans. They were found in most of Africa, across Eurasia from western Europe to India, and in the Americas from the Yukon to Peru.[5] The lion is a vulnerable species, having seen a major population decline in its African range of 30–50% per two decades during the second half of the 20th century.[2] Lion populations are untenable outside designated reserves and national parks. Although the cause of the decline is not fully understood, habitat loss and conflicts with humans are currently the greatest causes of concern. Within Africa, the West African lion population is particularly endangered." },
+                new Animal() { Id = 8, Name = "Elephant", Image = "/Assets/elephant.png", Description = "Elephants are large mammals of the family Elephantidae and the order Proboscidea. Two species are traditionally recognised, the African elephant (Loxodonta africana) and the Asian elephant (Elephas maximus), although some evidence suggests that African bush elephants and African forest elephants are separate species (L. africana and L. cyclotis respectively). Elephants are scattered throughout sub-Saharan Africa, South Asia, and Southeast Asia. Elephantidae is the only surviving family of the order Proboscidea; other, now extinct, members of the order include deinotheres, gomphotheres, mammoths, and mastodons. Male African elephants are the largest extant terrestrial animals and can reach a height of 4 m (13 ft) and weigh 7,000 kg (15,000 lb). All elephants have several distinctive features the most notable of which is a long trunk or proboscis, used for many purposes, particularly breathing, lifting water and grasping objects. Their incisors grow into tusks, which can serve as weapons and as tools for moving objects and digging. Elephants' large ear flaps help to control their body temperature. Their pillar-like legs can carry their great weight. African elephants have larger ears and concave backs while Asian elephants have smaller ears and convex or level backs." },
+                new Animal() { Id = 9, Name = "Shark", Image = "/Assets/shark.jpg", Description = "Sharks are a group of fish characterized by a cartilaginous skeleton, five to seven gill slits on the sides of the head, and pectoral fins that are not fused to the head. Modern sharks are classified within the clade Selachimorpha (or Selachii) and are the sister group to the rays. However, the term 'shark' has also been used for extinct members of the subclass Elasmobranchii outside the Selachimorpha, such as Cladoselache and Xenacanthus, as well as other Chondrichthyes such as the holocephalid eugenedontidans. Under this broader definition, the earliest known sharks date back to more than 420 million years ago.[1] Acanthodians are often referred to as 'spiny sharks'; though they are not part of Chondrichthyes proper, they are a paraphyletic assemblage leading to cartilaginous fish as a whole." }
+        };
+
+        public static List<Animal> GetAnimals()
+        {
+            return _animals;
+        }
+
+        public static Animal GetItemById(int id)
+        {
+            return _animals.FirstOrDefault(a => a.Id == id);
+        }
+    }
+}
